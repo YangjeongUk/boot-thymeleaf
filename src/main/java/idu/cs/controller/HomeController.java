1b845351ebda267eebd868c3d0cf4c9e2cc95e9f
YangJeongUk <yyybbq456@naver.com>
2019-05-22 15:18:05 +0900

    author committer setting

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -19,8 +19,7 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("test", "인덕 컴소");
-		model.addAttribute("egy", "유응구");
+
 		return "index";
 	}
 	@GetMapping("/register")
