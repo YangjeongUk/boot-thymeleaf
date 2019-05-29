ff79959802570b59707f80a046c2a6397b1212db
kimjinhyeok <hackertimoarm@gmail.com>
2019-05-29 21:12:47 +0900

    menu 수정 에노테이션

--- src/main/java/idu/cs/repository/UserRepository.java
+++ src/main/java/idu/cs/repository/UserRepository.java
@@ -3,11 +3,14 @@ package idu.cs.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import idu.cs.domain.User;
 
+@Repository
 public interface UserRepository 
-	extends JpaRepository<User, Long> {
-	public List<User> findByName(String name);
-
+	extends JpaRepository<User, Long> { //검색할 때 필요한 부분
+	List<User> findByName(String name);
+	List<User> findByCompany(String company);
+	User findByUserId(String userId); //ByUserId== where userId='' 
 }
